b41e033fae12b119dd3c16a68ec00fbd2af324ad
Joao-Filho19 <joaossfbae@gmail.com>
2022-08-26 20:43:52 -0400

    primeiro commit

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacao;
+
+import entidade.Retangulo;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc=new Scanner(System.in);
+        Retangulo r1= new Retangulo();
+        
+        System.out.println("Informe a altura do retangulo");
+        r1.altura=sc.nextDouble();
+        
+        System.out.println("Informe a largura do retangulo");
+        r1.largura=sc.nextDouble();
+        
+        System.out.printf("Area: %.2f\n",r1.areaRetangulo());
+        System.out.printf("Area: %.2f\n",r1.perimetroRetangulo());
+        System.out.printf("Area: %.2f\n",r1.diagonalRetangulo());
+        
+    }
+    
+}
